bc7fd16fd28d32fa7d9d5f46cd06ce87f9c1eb76
Kai Curry <kai@cape.io>
2017-06-26 18:39:56 -0400

    expand messages

--- src/messages.js
+++ src/messages.js
@@ -1,20 +1,182 @@
 export default [
-  'We’re okay! Everyone is safe and sound. All systems operational.',
-  'Running behind schedule. No worries.',
-  'Decided to stay another day or more.',
-  'Leaving tomorrow or when the weather is good.',
-  'Tired but otherwise fine.',
-  'Fixing something that broke. Nothing critical, just want to take care of it.',
-  'Severely compromised equipment or failure. Issues with the sailing rig.',
-  'Severely compromised equipment or failure. Issues with the diesel engine.',
-  'Severely compromised equipment or failure. Issues with the sailing rig and diesel engine.',
-  'Waiting for better weather more/less wind.',
-  'There is strong strong weather and we are uncomfortable.',
-  'Helping someone else.',
-  'Hungry. Running low on food.',
-  'We have help on the way. In contact with them.',
-  'Hard Aground.',
-  'Taking on water.',
-  'Fire onboard',
-  'Medical/ Health Issues',
+  {
+    id: 0,
+    msg: 'We’re okay! Everyone is safe and sound. All systems operational.',
+    status: 'ok',
+  },
+  {
+    id: 1,
+    msg: 'We miss you all.',
+    status: 'ok',
+  },
+  {
+    id: 2,
+    msg: 'It is a rainy, rainy day so we are going to play a card game.',
+    status: 'ok',
+  },
+  {
+    id: 3,
+    msg: 'Keeping watch all night. Dangerous area.',
+    status: 'warning',
+  },
+  {
+    id: 4,
+    msg: 'Found some friends!',
+    status: 'ok',
+  },
+  {
+    id: 5,
+    msg: 'Arrived at our destination safely.',
+    status: 'ok',
+  },
+  {
+    id: 6,
+    msg: 'Still alive.',
+  },
+  {
+    id: 7,
+    msg: 'Hello! This is a test.',
+  },
+  {
+    id: 8,
+    msg: 'Running behind schedule. No worries.',
+  },
+  {
+    id: 9,
+    msg: 'On the way.',
+  },
+  {
+    id: 10,
+    msg: 'Decided to stay another day or two.',
+  },
+  {
+    id: 11,
+    msg: 'Decided to stay another week.',
+  },
+  {
+    id: 12,
+    msg: 'Leaving tomorrow or when the weather is good.',
+  },
+  {
+    id: 13,
+    msg: 'Waiting for better weather more/less wind.',
+  },
+  {
+    id: 14,
+    msg: 'Really stormy weather out here.',
+  },
+  {
+    id: 15,
+    msg: 'Tired but otherwise fine.',
+  },
+  {
+    id: 16,
+    msg: 'Fixing something that broke. Nothing critical, just want to take care of it.',
+  },
+  {
+    id: 17,
+    msg: 'Having issues with the sailing rig.',
+  },
+  {
+    id: 18,
+    msg: 'Having issues with the diesel engine.',
+  },
+  {
+    id: 19,
+    msg: 'Severely compromised equipment or failure. Issues with the sailing rig and diesel engine.',
+  },
+  {
+    id: 20,
+    msg: 'There is strong strong weather and we are uncomfortable.',
+  },
+  {
+    id: 21,
+    msg: 'Helping someone else.',
+  },
+  {
+    id: 22,
+    msg: 'Hungry. Running low on food.',
+  },
+  {
+    id: 23,
+    msg: 'We have help on the way. In contact with them.',
+  },
+  {
+    id: 24,
+    msg: 'Hard Aground. Stuck.',
+    status: 'danger',
+  },
+  {
+    id: 25,
+    msg: 'Taking on water. Bilge pump taking care of it.',
+  },
+  {
+    id: 26,
+    msg: 'Taking on water. Overwhelming bilge pumps.',
+  },
+  {
+    id: 27,
+    msg: 'Fire onboard.',
+  },
+  {
+    id: 28,
+    msg: 'Medical/ Health Issues.',
+  },
+  {
+    id: 29,
+    msg: 'We have everything more or less under control.',
+  },
+  {
+    id: 30,
+    msg: 'We are requesting help elsewhere.',
+  },
+  {
+    id: 31,
+    msg: 'We have help on the way. In contact with them.',
+  },
+  {
+    id: 32,
+    msg: 'We need help and have not found it yet. Please coordinate with authorities.',
+  },
+  {
+    id: 33,
+    msg: 'Yep, anchor is set and we checked it with the look bucket.',
+  },
+  {
+    id: 34,
+    msg: 'Issues with power. Low on battery.',
+  },
+  {
+    id: 35,
+    msg: 'Refridgerator broken down. Chow time.',
+  },
+  {
+    id: 36,
+    msg: 'Waiting for coastguard rescue.',
+    status: 'emergency',
+  },
+  {
+    id: 37,
+    msg: 'Major communication systems down.',
+    status: 'danger',
+  },
+  {
+    id: 38,
+    msg: 'Kai is without a phone. Call Emily if needed.',
+    status: 'status',
+  },
+  {
+    id: 39,
+    msg: 'Emily is without a phone. Call Kai if needed.',
+    status: 'status',
+  },
+  {
+    id: 40,
+    msg: 'Deployed EPIRB.',
+    status: 'emergency',
+  },
+  {
+    id: 41,
+    msg: 'Cold front passage. Much rain and wind shifting.',
+  },
 ]
