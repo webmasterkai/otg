efb595f29014d1ec2fc80becb3cac70529b0d56a
Kai Curry <kai@cape.io>
2017-06-30 23:02:26 -0400

    add state select for props

---
+++ src/select.js
@@ -0,0 +1,19 @@
+import { over }
+// 115 STNG Converter
+// 130 RS130 GPS
+
+export const getPos = get('data.data.130.129025.fields')
+export const getLat = flow(getPos, get('latitude'))
+export const getLon = flow(getPos, get('longitude'))
+
+export const getPosArr = over([getLon, getLat]) (state) {
+
+}
+
+export const selector = {
+  'position': getPositionArray,
+}
+'data.data.130.129025.fields.latitude'
+'data.data.130.129025.fields.longitude'
+'data.data.130.129026.fields.cog'
+'data.data.130.129026.fields.sog'
